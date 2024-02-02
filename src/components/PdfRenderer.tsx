36733195d411d6cc3a76668cfd85877dd05a808b
CloudGod5 <Jtingley2021@gmail.com>
2024-02-02 14:07:33 -0600

    PDF loading, and Page number view/function

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { ChevronDown, Loader2 } from 'lucide-react';
+import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf'
-
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import { useToast } from './ui/use-toast';
 import { useResizeDetector } from 'react-resize-detector';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { error } from 'console';
+import { cn } from '@/lib/utils';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -19,22 +24,93 @@ interface PdfRendererProps {
 const PdfRenderer = ({url}: PdfRendererProps) => {
 
     const {toast} = useToast()
-
     const { width, ref } = useResizeDetector();
+    const [numPages, setNumPages] = useState<number>()
+    const [currPage, setCurrPage] = useState<number>(1)
+
+    const CustomPageValidator = z.object({
+        page: z
+          .string()
+          .refine(
+            (num) => Number(num) > 0 && Number(num) <= numPages!
+          ),
+      })
+
+      type TCustomPageValidator = z.infer<
+        typeof CustomPageValidator
+    >
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue,
+      } = useForm<TCustomPageValidator>({
+        defaultValues: {
+          page: '1',
+        },
+        resolver: zodResolver(CustomPageValidator),
+      })
+    
+      const handlePageSubmit = ({
+        page,
+      }: TCustomPageValidator) => {
+        setCurrPage(Number(page))
+        setValue('page', String(page))
+      }
 
     return (
         <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
             <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-                <div className="flex items-center gap-1.5">
-                    <Button variant='ghost' aria-label='previous page'>
-                        <ChevronDown className='h-4 w-4'/>
-                    </Button>
+            <div className='flex items-center gap-1.5'>
+            <Button
+                disabled={currPage <= 1}
+                onClick={() => {
+                setCurrPage((prev) =>
+                    prev - 1 > 1 ? prev - 1 : 1
+                )
+                setValue('page', String(currPage - 1))
+                }}
+                variant='ghost'
+                aria-label='previous page'>
+                <ChevronUp className='h-4 w-4' />
+            </Button>
 
-                    <div className='flex items-center gap-1.5'>
-                        <Input className='w-12 h-8' />
-                        <p className='text-zinc-700'></p>
-                    </div>
-                </div>
+            <div className='flex items-center gap-1.5'>
+                <Input
+                {...register('page')}
+                className={cn(
+                    'w-12 h-8',
+                    errors.page && 'focus-visible:ring-red-500'
+                )}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                    handleSubmit(handlePageSubmit)()
+                    }
+                }}
+                />
+                <p className='text-zinc-700 text-sm space-x-1'>
+                <span>/</span>
+                <span>{numPages ?? '--'}</span>
+                </p>
+            </div>
+
+            <Button
+                disabled={
+                numPages === undefined ||
+                currPage === numPages
+                }
+                onClick={() => {
+                setCurrPage((prev) =>
+                    prev + 1 > numPages! ? numPages! : prev + 1
+                )
+                setValue('page', String(currPage + 1))
+                }}
+                variant='ghost'
+                aria-label='next page'>
+                <ChevronDown className='h-4 w-4' />
+            </Button>
+            </div>
             </div>
             <div className="flex-1 w-full max-h-screen">
                 <div ref={ref}>
@@ -48,8 +124,13 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
                             description: 'There was an error loading the PDF. Please try again later.',
                             variant: 'destructive'
                         })
-                    }} file={url} className="max-h-full">
-                        <Page width={width ? width : 1} pageNumber={1} />
+                    }} 
+                    onLoadSuccess={({numPages}) => setNumPages(numPages)}
+                    file={url} className="max-h-full">
+                        <Page 
+                        width={width ? width : 1} 
+                        pageNumber={currPage} 
+                        />
                     </Document>
                 </div>
             </div>
