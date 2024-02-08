73fcebc8e81c4a2551eac8eb16ac3e46ac5a4d44
CloudGod5 <Jtingley2021@gmail.com>
2024-02-08 10:58:41 -0600

    Update route.ts

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,15 +1,3 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest, NextResponse } from 'next/server'
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: any
-): Promise<NextResponse> {
-  const endpoint = params.kindeAuth
-  const handler = await handleAuth(request, endpoint)
-  if (typeof handler === 'function') {
-    return handler(request, new NextResponse())
-  } else {
-    return handler
-  }
-}
+export const GET = handleAuth();
