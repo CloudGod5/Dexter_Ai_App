d35d094ee0152ec55bc373aa30a0aadba9738f95
CloudGod5 <Jtingley2021@gmail.com>
2024-02-08 14:30:26 -0600

    wuat

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,22 +1,6 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const GET = async (req: any, res: any) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  return handleAuth(req, res);
-}
-
-const handler = async (req: any, res: any) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  return handleAuth(req, res);
-}
-
-export { handler as POST }
+export const GET = handleAuth();
 
 
 // import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
