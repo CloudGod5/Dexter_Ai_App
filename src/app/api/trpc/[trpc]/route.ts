a4c81f327b1a9586bbc0ff5c55a99b3417b33673
CloudGod5 <Jtingley2021@gmail.com>
2024-02-08 14:01:31 -0600

    Update route.ts

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,33 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
-import { appRouter } from '@/trpc'
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const handler = (req: Request) =>
-  fetchRequestHandler({
-    endpoint: '/api/trpc',
-    req,
-    router: appRouter,
-    createContext: () => ({}),
-  })
+export const GET = async (req: any, res: any) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-export { handler as GET, handler as POST }
+  return handleAuth(req, res);
+}
+
+const handler = async (req: any, res: any) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  return handleAuth(req, res);
+}
+
+export { handler as POST }
+
+
+// import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+// import { appRouter } from '@/trpc'
+
+// const handler = (req: Request) =>
+//   fetchRequestHandler({
+//     endpoint: '/api/trpc',
+//     req,
+//     router: appRouter,
+//     createContext: () => ({}),
+//   })
+
+// export { handler as GET, handler as POST }
